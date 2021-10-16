31830993f2ed35187163fb847530df7184225213
Arlauskas Arminas <PG00156LA>
2021-10-16 12:38:56 +0300

    Revert "Update main.cpp"
    This reverts commit 798899c83e498c2f851d1cc94d43b6ea8bdc78c7.

--- Katino vakariene/Katino vakariene/main.cpp
+++ Katino vakariene/Katino vakariene/main.cpp
@@ -5,58 +5,50 @@
 using namespace std;
 
 const char CDfv[] = "Duom.txt";
-const int _cmax = 40;
-
-void Input(const char fv[], int &amount, int Taste[], float Weight[]);
-void Solution(int amount, int Taste[], float Weight[], int &good, float &sum, int &tasty, int &big);
-void Output(int good, float sum, int tasty, int big);
-
+const int Cmax = 40;
+//------------------------------------
+void Ivestis(const char fv[], int &k, int Skon[], float Svor[]);
+void Sprendimas(int k, int Skon[], float Svor[], int &kiek, float &sum, int &skan, int &did);
+void Isvestis(int kiek, float sum, int skan, int did);
+//************************************
 int main() {
-	int amount = 0;
-	int Taste[_cmax];
-	float Weight[_cmax];
-	int good = 0;
-	int tasty = 0;
-	int big = 0;
-	float sum = 0;
-
-	Input (CDfv, amount, Taste, Weight);
-	Solution (amount, Taste, Weight, good, sum, tasty, big);
-	Output (good, sum, tasty, big);
+	int k=0, Skon[Cmax];
+	float Svor[Cmax];
+	Ivestis(CDfv, k, Skon, Svor);
+	int kiek=0, skan=0, did=0;
+	float sum=0;
+	Sprendimas(k, Skon, Svor, kiek, sum, skan, did);
+	Isvestis(kiek, sum, skan, did);
 }
-
-void Input (const char fv[], int &amount, int Taste[], float Weight[]) {
-	char answer = 'T';
+//************************************
+void Ivestis(const char fv[], int &k, int Skon[], float Svor[]) {
+	char ats = 'T';
 	cout << "Iveskite zuvies svori ir skoni: " << endl;
-
-	while (answer == 'T') {
-		cin >> Weight[amount];
-		cin >> Taste[amount];
-
-		amount++;
+	while (ats == 'T') {
+		cin >> Svor[k];
+		cin >> Skon[k];
+		k++;
 		cout << "Ar dar liko zuveliu? ";
-		cin >> answer;
+		cin >> ats;
 	}
 }
-
-void Solution (int amount, int Taste[], float Weight[], int &good, float &sum, int &tasty, int &big) {
-	for (int i = 0; i < amount; i++) {
-		
-		if (Taste[i] == 1 && Weight[i] >= 1) {
-			good++;
-			sum += Weight[i];
+//------------------------------------
+void Sprendimas(int k, int Skon[], float Svor[], int &kiek, float &sum, int &skan, int &did) {
+	for (int i = 0; i < k; i++) {
+		if (Skon[i] == 1 && Svor[i] >= 1) {
+			kiek++;
+			sum += Svor[i];
 		}
-		if (Taste[i] == 1) {
-			tasty++;
+		if (Skon[i] == 1) {
+			skan++;
 		}
-		if (Weight[i] >= 1) {
-			big++;
+		if (Svor[i] >= 1) {
+			did++;
 		}
-	
 	}
 }
-
-void Output (int good, float sum, int tasty, int big ) {
-	cout << good << " " << fixed << setprecision(2) << sum << endl;
-	cout << "Dideliu zuveliu - " << big << ", skaniu - " << tasty;
+//------------------------------------
+void Isvestis(int kiek, float sum, int skan, int did ) {
+	cout << kiek << " " << fixed << setprecision(2) << sum << endl;
+	cout << "Dideliu zuveliu - " << did << ", skaniu - " << skan;
 }
